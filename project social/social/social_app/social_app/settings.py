2642ceba187960aa1a97b0ba285a5fdbcb2cb059
ArgyrisApergis <argyris.apergis@gmail.com>
2022-04-26 14:34:01 +0300

    styles ok3/ deb false/allhosts '*'

--- project social/social/social_app/social_app/settings.py
+++ project social/social/social_app/social_app/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-e@h(7ul+jl9b29on-+4hcl&ijr^icph5e(+o*i!%j80$-1k^s_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
